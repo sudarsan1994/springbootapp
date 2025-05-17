62fc0ab40acd314e3dc77810f1dd9abc1f05d996
Sudarshan <Sudarshan1994@yahoo.com>
2025-05-17 08:26:49 +0530

    first commit

---
+++ Dockerfile
@@ -0,0 +1,11 @@
+# Use an OpenJDK 17 base image
+FROM eclipse-temurin:17-jdk-alpine
+
+# Set the working directory
+WORKDIR /app
+
+# Copy the jar file (assumes it was built by Maven)
+COPY build/libs/*.jar app.jar
+
+# Run the Spring Boot application
+ENTRYPOINT ["java", "-jar", "app.jar"]
---
+++ src/main/java/com/example/springbootapp/Test.java
@@ -0,0 +1,14 @@
+package com.example.springbootapp;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Test {
+
+    @GetMapping("/greet")
+    public String greet(){
+        return "Hello world!!!!!!!!!!!!!!!";
+    }
+}
+
